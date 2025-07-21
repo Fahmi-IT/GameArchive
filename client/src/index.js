95f4ba89239195fc4531d526a329dbc3dea85354
Fahmi Sajid Ahmed <sajidfahmi03@gmail.com>
2025-07-20 23:54:24 -0400

    added .jsx

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { CompareProvider } from './contexts/CompareContext';
+import { CompareProvider } from './contexts/CompareContext.jsx';
 import { LanguageProvider } from './contexts/LanguageContext.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
